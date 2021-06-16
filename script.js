a2023257ea90e3fad18fb3559389fd0edbed51a9
Ajaykumar18103 <85347630+Ajaykumar18103@users.noreply.github.com>
2021-06-16 15:46:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ function compute()
     var q=document.getElementById("principal").value;
     var r=document.getElementById("years").value;
   
-  document.getElementById("result").innerHTML="If you deposit "+q+","+\n+"at an interest rate of "+p+"."+\n+"You will receive an amount of "+((q*r*p)/100);
+  document.getElementById("result").innerHTML="If you deposit "+q+",\nat an interest rate of "+p+".\nYou will receive an amount of "+((q*r*p)/100)+",\nin the year "+r;
     
 }    
