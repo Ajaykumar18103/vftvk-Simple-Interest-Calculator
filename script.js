a2005adac3acf0f310058ec8a0d3e6336adcbf8d
Ajaykumar18103 <85347630+Ajaykumar18103@users.noreply.github.com>
2021-06-27 23:54:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function compute()
     p=document.getElementById("rate").value;
     q=document.getElementById("principal").value;
     r=document.getElementById("years").value;
-        if(q<=0 && q=" "){
+        /*if(q<=0 && q=" "){
             alert("Enter a Positive Number");
             q.focus();
-        }
+        }*/
 
     
     document.getElementById("result").innerHTML="If you deposit "+q+",<br>at an interest rate of "+p+".<br>You will receive an amount of "+((p*r*q)/100)+",<br>in the year";
