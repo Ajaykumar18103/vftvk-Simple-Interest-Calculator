e54323d315ad76ddffed55726ce9ff026593b483
Ajaykumar18103 <85347630+Ajaykumar18103@users.noreply.github.com>
2021-06-27 23:23:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function compute()
     let q=document.getElementById("principal").value;
     let r=document.getElementById("years").value;
     let s=p*q*r;
-        if(q<=0 && q=" "){
+        /*if(q<=0 && q=" "){
             alert("Enter a Positive Number");
             q.focus();
-        }
+        }*/
 
     
     document.getElementById("result").innerHTML="If you deposit "+q+",<br>at an interest rate of "+p+".<br>You will receive an amount of "+s+",<br>in the year";
