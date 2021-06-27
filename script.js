17f3b32281b4e1e79112e2791cdc9265185495fd
Ajaykumar18103 <85347630+Ajaykumar18103@users.noreply.github.com>
2021-06-27 23:50:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,6 @@ function compute()
         }else{
 
     
-    document.getElementById("result").innerHTML="If you deposit "+q+",<br>at an interest rate of "+p+".<br>You will receive an amount of "+(p*r*q)+",<br>in the year";
+    document.getElementById("result").innerHTML="If you deposit "+q+",<br>at an interest rate of "+p+".<br>You will receive an amount of "+((p*r*q)/100)+",<br>in the year";
     }
     }
